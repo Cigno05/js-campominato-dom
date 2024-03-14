7a9154995361e1b2df50373c58e4db04c85276a4
Cigno05 <paolocalcagni.82@gmail.com>
2024-03-14 17:42:57 +0100

    ADD: bonus 1 and 2

--- js/app.js
+++ js/app.js
@@ -7,23 +7,24 @@ const playButton = document.getElementById('play-btn');
 playButton.addEventListener('click', playTheGame)
 
 //creare la funzione per l'event listner
-function playTheGame () {
+function playTheGame() {
     //richiamo l'elemento in cui andrá il div .cell
     let gridElement = document.querySelector('.grid-cells');
     // console.log(gridElement);
     const difficultyChoice = document.getElementById('difficult-select');
-    console.log(difficultyChoice.value)
+    // console.log(difficultyChoice.value)
 
     //stabilisco quante celle sono presenti in una riga
     // let size = parseInt(difficultyChoice.value);
     let size = levelGame(difficultyChoice.value);
-    
-    
+
+
     // stabilisco il numero di celle presenti nella griglia
     let numberOfCells = size * size;
-    
-    let bombs = bombsGenerator(1, numberOfCells, 16 );
+
+    let bombs = bombsGenerator(1, numberOfCells, 16);
     console.log(bombs) //array
+
 
     // // gridElement vuoto per non accumulare la generazione di cartelle
     gridElement.innerHTML = '';
@@ -32,7 +33,7 @@ function playTheGame () {
     for (let index = 0; index < numberOfCells; index++) {
         const number = index + 1;
         // console.log(number);
-        
+
         // creo l'elemento che andró a mettere dentro gridElement
         const cellElement = document.createElement('div');
         // aggiungo la classe al div
@@ -41,30 +42,32 @@ function playTheGame () {
         cellElement.className += ` cell-${size}`;
         //aggiungo il numero a ogni cell
         cellElement.innerHTML = number;
-        
+
         //aggiungo l'elemento creato al gridElement
         gridElement.append(cellElement);
-        
+
         //assegno una classe cambia colore all'elemento al momento del click
-        cellElement.addEventListener('click', function() {
+        cellElement.addEventListener('click', function () {
             // console.log('click on ', number)
 
             //aggiungo/tolgo la classe all'elemento cliccato
             cellElement.classList.add('bg-dark');
-            cellElement.classList.add('selected');
             // console.log(cellElement)
 
             //conteggio celle selezionate prima della bomba
-            let howManySelectedCell = document.getElementsByClassName('selected').length
+            let howManySelectedCell = document.getElementsByClassName('bg-dark').length
             console.log(howManySelectedCell);
             //mettere le celle che hanno la classe 'bg-dark' in un array
-            
+
             // se l'array includ() il numero della cella 
             if (bombs.includes(number)) {
-                    // cella diventa rossa
+                // tutte le celle diventano rosse
                 cellElement.classList.remove('bg-dark');
-                cellElement.classList.remove('selected');
-                cellElement.classList.add('bg-alert');
+
+
+                //assegnare la classe 'bg-alert' a tutti gli elementi che hanno number uguale a bombs[i]
+
+
                 gridElement.classList.add('finish-game');
                 gridElement.innerHTML += `
                 <div>
@@ -72,8 +75,26 @@ function playTheGame () {
                     <h2>Il tuo punteggio é ${howManySelectedCell - 1}</h2>
                 </div>
                 `;
-            } 
-            
+
+                alert('BOOM!!!');
+                const cellDomElements = document.querySelectorAll('.cell');
+
+                for (let index = 0; index < cellDomElements.length; index++) {
+                    const cellValue = parseInt(cellDomElements[index].innerHTML)
+                    if (bombs.includes(cellValue)) {
+                        cellDomElements[index].classList.add('bg-alert');
+                    }
+                }
+
+                // for (let index = 0; index < bombs.length; index++) {
+
+                //     const bomb = bombs[index]
+                //     cellDomElements[bomb - 1].classList.add('bg-alert');
+                // }
+
+
+            }
+
         })
     }
 }
@@ -97,7 +118,7 @@ function levelGame(value) {
 }
 
 // funzione genera array lungo lenght con numeri diversi da min a max e return array
-function bombsGenerator (min, max, lenght) {
+function bombsGenerator(min, max, lenght) {
 
     const randomViwed = [];
 
@@ -105,7 +126,7 @@ function bombsGenerator (min, max, lenght) {
 
     //lughezza dell'array < a quanti numeri voglio nell'array 
     while (randomViwed.length < lenght) {
-        
+
         // genero numero casuale
         const randomNumber = Math.floor(Math.random() * max) + min;
 
@@ -113,8 +134,8 @@ function bombsGenerator (min, max, lenght) {
 
         let found = false;
         //scorro tra i numeri generati in precedenza
-        for (let index2 = 0; index2 < randomViwed.length; index2++) {
-            if (randomViwed[index2] === randomNumber) {
+        for (let index = 0; index < randomViwed.length; index++) {
+            if (randomViwed[index] === randomNumber) {
                 found = true;
             }
         }
@@ -124,16 +145,17 @@ function bombsGenerator (min, max, lenght) {
             randomViwed.push(randomNumber)
         }
         // inserisco il numero nell'array
-    
+
     }
     // ritorna l'array
     return randomViwed
 }
 
-//prendere celle selezionate e metterle mette in un array
-    //stampo la lunghezza dell'array
-    
-   
+// aggiungere class2 agli elementi che hanno class1
+
+
+
+
 
 
 
